05589bf73210c7d546cdfca26687929bd3b0177c
Akash Sengupta <52875362+akash-sgta@users.noreply.github.com>
2021-04-10 13:12:32 +0530

    phase 3 changes to outliers and dev credits

---
+++ contributors/Joyjit_Ghosh.md
@@ -0,0 +1,11 @@
+# Tester
+
+## Joyjit Ghosh
+
+### joyjitghosh98@gmail.com
+
+### +91-NONE
+
+### <https://github.com/JOYJIT43>
+
+### BTech Computer Science and Engineering (2017-2021)
--- outiers/development_2.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-# -----------------------------------------------
-
-DB_URL = os.path.join(Path(__file__).resolve().parent.parent, 'test.sqlite3')
-TG_TOKEN = '1470401355:AAErhMgyyWSAerh_JfN7BkdPwP37D1dl00U'
-DEBUG = True
