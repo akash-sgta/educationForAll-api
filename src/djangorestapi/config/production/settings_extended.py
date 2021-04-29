c44447bd92e0fe49e93afdc861031303f39c9a30
Akash Sengupta <akashofficial1998@gmail.com>
2021-04-30 00:16:38 +0530

    health check

--- src/djangorestapi/config/production/settings_extended.py
+++ src/djangorestapi/config/production/settings_extended.py
@@ -28,6 +28,11 @@ DATABASE_ROUTERS = (
     'routers.db_routers.App_Router',
 )
 
+CRED = (
+    ("JCGbVPW0pV", "wyJF99q5ym"),
+    ("xNYSHtH8zf", "pKCUPv0Xho")
+)
+
 DB_SETTINGS = dict()
 DB_SETTINGS['default'] = {}
 DB_SETTINGS['auth_db'] = {
@@ -35,20 +40,20 @@ DB_SETTINGS['auth_db'] = {
     'OPTIONS': {
         'sql_mode': 'traditional',
     },
-    'NAME':  "djangoauthDB",
-    'USER': "admin",
-    'PASSWORD': "GANDUgandu",
-    'HOST': "projectdatabase1.czbsimzcrcxe.ap-south-1.rds.amazonaws.com",
-    'PORT': "6969"
+    'NAME':  CRED[0][0],
+    'USER': CRED[0][0],
+    'PASSWORD': CRED[0][1],
+    'HOST': "remotemysql.com",
+    'PORT': "3306"
 }
 DB_SETTINGS['app_db'] = {
     'ENGINE': "django.db.backends.mysql",
     'OPTIONS': {
         'sql_mode': 'traditional',
     },
-    'NAME':  "appDB",
-    'USER': "admin",
-    'PASSWORD': "GANDUgandu",
-    'HOST': "projectdatabase1.czbsimzcrcxe.ap-south-1.rds.amazonaws.com",
-    'PORT': "6969"
+    'NAME':  CRED[1][0],
+    'USER': CRED[1][0],
+    'PASSWORD': CRED[1][1],
+    'HOST': "remotemysql.com",
+    'PORT': "3306"
 }
