e6be9a73deb50fd58b3e8e661276fbbc97022997
Akash Sengupta <akashofficial1998@gmail.com>
2021-04-29 21:27:46 +0530

    minute changes

--- src/djangorestapi/djangorestapi/settings.py
+++ src/djangorestapi/djangorestapi/settings.py
@@ -68,6 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'djangorestapi.urls'
