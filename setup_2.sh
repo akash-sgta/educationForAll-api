52901fd8c314862eb773f8185ce92e3b10fb7f2d
Akash Sengupta <alphatrionaksh@gmail.com>
2021-05-25 00:23:11 +0530

    alpha

--- setup_2.sh
+++ setup_2.sh
@@ -3,8 +3,9 @@
 ~/education-for-all/venv/bin/python3 ~/education-for-all/src/djangorestapi/manage.py migrate --database=app_db
 echo "\n"
 echo "Do you want to automatically create a superuser ? [y/n]"
-read -p ans
-if [ "$ans" = "y" ]; then
+read ans
+if [ ans == "y" ]
+then
     python3 manage.py createsuperuser --database=auth_db
 else
     echo "All set. Only a few things are left to assemble."
