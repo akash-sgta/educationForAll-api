564ccd51efc38ffbcfa36b300dbe1fa9570a3668
Akash Sengupta <alphatrionaksh@gmail.com>
2021-05-24 23:06:05 +0530

    silly mistakes

--- src/djangorestapi/runme.sh
+++ src/djangorestapi/runme.sh
@@ -1,15 +1,10 @@
 sudo apt install python3 python3-pip python3-dev libmysqlclient-dev build-essential gcc nginx git
 python3 -m pip install virtualenv
 
-cd ~
-mkdir repositories
-cd repositories
-git clone https://github.com/akash-sgta/education-for-all.git
-
-cd ~/repositories/education-for-all/
+cd education-for-all
 python3 -m virtualenv venv
 source venv/bin/activate
-cd ~/repositories/education-for-all/src/djangorestapi
+cd src/djangorestapi
 
 python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations auth_prime analytics user_personal content_delivery cronjobs
@@ -20,8 +15,8 @@ sleep 8
 python3 manage.py createsuperuser --database=auth_db
 
 # Creating symbolic links
-ln -s ~/repositories/education-for-all/src/djangorestapi/config/uwsgi.ini ~/uwsgi.ini
-ln -s~/repositories/education-for-all/src/djangorestapi/config/manage.py ~/manage.py
+ln -s config/uwsgi.ini ~/uwsgi.ini
+ln -s manage.py ~/manage.py
 
 echo "Setting up nginx or apache server will have to be done manually"
 sleep 2
